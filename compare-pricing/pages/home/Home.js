e6e75d02b8c089813ba5327190b4aa844fefa49d
Nadia Fr.Lm <84836971+NadiaFrShLm@users.noreply.github.com>
2023-03-08 13:53:58 +0100

    Add files via upload

---
+++ compare-pricing/pages/home/Home.js
@@ -0,0 +1,10 @@
+import ComparePricing from '../../src/components/comparePricing'
+import stl from './Home.module.scss'
+
+const Home = () => (
+  <div className={stl.wrapper}>
+    <ComparePricing />
+  </div>
+)
+
+export default Home
